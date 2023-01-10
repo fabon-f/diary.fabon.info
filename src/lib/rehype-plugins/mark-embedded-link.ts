71f6cd6e39e977ab3d8d64a4fa2fb75e1db643c8
fabon <syobon.hinata.public@gmail.com>
2023-01-11 05:23:31 +0900

    Implement link card

--- src/lib/rehype-plugins/mark-embedded-link.ts
+++ src/lib/rehype-plugins/mark-embedded-link.ts
@@ -2,13 +2,19 @@ import { visit } from 'unist-util-visit'
 
 export default function plugin() {
     function transformer(tree: any) {
-        visit(tree, { type: 'element', tagName: 'a' }, n => {
-            if (!Array.isArray(n.children)) { return }
-            if (n.children.length !== 1) { return }
-            if (n.children[0].type !== 'text') { return }
-            if (n.properties.href === n.children[0].value) {
-                n.properties['dataEmbedLink'] = ''
+        visit(tree, { type: 'element', tagName: 'p' }, (node, index, parent) => {
+            if (index === null) { return }
+            if (!Array.isArray(node.children)) { return }
+            if (node.children.length !== 1) { return }
+            const child = node.children[0]
+            if (child.tagName !== 'a') { return }
+            if (!Array.isArray(child.children)) { return }
+            if (child.children.length !== 1) { return }
+            if (child.children[0].type !== 'text') { return }
+            if (child.properties.href === child.children[0].value) {
+                child.properties['dataEmbedLink'] = ''
             }
+            parent.children[index] = child
         })
     }
     return transformer
