58ae991cb5d364ede10d65e2797109ba78cc5af7
fabon <syobon.hinata.public@gmail.com>
2023-01-12 20:05:29 +0900

    Improve syntax highlighting

--- astro.config.ts
+++ astro.config.ts
@@ -9,19 +9,29 @@ import remarkDendenRuby from 'remark-denden-ruby'
 import remarkFootnoteTitle from './src/lib/remark-plugins/footnote-title'
 import rehypeKatex from 'rehype-katex'
 import rehypeImgSize from 'rehype-img-size'
+import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeEmbeddedLink from './src/lib/rehype-plugins/mark-embedded-link'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx(), sitemap()],
     markdown: {
+        syntaxHighlight: false,
         remarkPlugins: [
             remarkGfm, remarkMath, remarkBreaks, remarkDendenRuby, remarkFootnoteTitle,
         ],
         rehypePlugins: [
             rehypeKatex,
             [rehypeImgSize as any, { dir: 'public' }],
-            rehypeEmbeddedLink
+            rehypeEmbeddedLink,
+            [rehypePrettyCode, {
+                theme: 'nord', keepBackground: true,
+                onVisitLine(node: any) {
+                    if (node.children.length === 0) {
+                        node.children = [{ type: 'text', value: ' ' }]
+                    }
+                }
+            }]
         ]
     },
     site: "https://diary.fabon.info"
